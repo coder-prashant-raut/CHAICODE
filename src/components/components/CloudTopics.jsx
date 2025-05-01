9b218c924e0042a8ba277bdf2682cf6e21c15ae2
Prashant Raut <prashantraut1308@gmail.com>
2025-05-01 13:44:52 +0530

    smooth hover in cloud topics

--- src/components/components/CloudTopics.jsx
+++ src/components/components/CloudTopics.jsx
@@ -1,23 +1,6 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-
-
-const topics = [
-  { name: "JavaScript", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", videoUrl: "https://www.youtube.com/watch?v=Hr5iLG7sUa0" },
-  { name: "Python", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg", videoUrl: "https://www.youtube.com/watch?v=Ca5DLSDfPec" },
-  { name: "Next.js", logo: "https://cdn.worldvectorlogo.com/logos/nextjs-2.svg", videoUrl: "https://www.youtube.com/watch?v=OgS1ZWZItno" },
-  { name: "Django", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg", videoUrl: "https://www.youtube.com/watch?v=j6szNSzw4BU" },
-  { name: "Docker", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg", videoUrl: "https://www.youtube.com/watch?v=rr9cI4u1_88" },
-  { name: "React.js", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", videoUrl: "https://www.youtube.com/watch?v=vz1RlUyrc3w" },
-  { name: "Kubernetes", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg", videoUrl: "https://www.youtube.com/watch?v=7XDeI5fyj3w" },
-  { name: "Node.js", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg", videoUrl: "https://www.youtube.com/watch?v=ooBxSg1Cl1w" },
-  { name: "HTML5", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", videoUrl: "https://www.youtube.com/watch?v=XmLOwJHFHf0" },
-  { name: "CSS3", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", videoUrl: "https://www.youtube.com/watch?v=aLzfFJb8rWo" },
-  { name: "Java", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg", videoUrl: "https://www.youtube.com/watch?v=Ev7-pchAFRM" },
-  { name: "C++", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg", videoUrl: "https://www.youtube.com/watch?v=gCkPJTSZ9mU" },
-  { name: "GoLang", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg", videoUrl: "https://www.youtube.com/watch?v=JoJ8Sw5Yb4c" },
-  { name: "Minecraft Server", logo: "https://cdn-icons-png.flaticon.com/512/5968/5968756.png", videoUrl: "https://www.youtube.com/watch?v=dZyQNy3-HjU" },
-];
+import topics from "../../data/topics";
 
 const row1 = topics.slice(0, 5);
 const row2 = topics.slice(5, 10);
@@ -98,12 +81,16 @@ const CloudTopics = () => {
         </p>
       </div>
 
-      <div className="relative overflow-hidden w-[95%] xs:w-[80%] mx-auto py-10 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-        <TopicRow items={row1} reverse={false} setHovered={setHoveredTopic} />
-        <TopicRow items={row2} reverse={true} setHovered={setHoveredTopic} />
-        <TopicRow items={row3} reverse={false} setHovered={setHoveredTopic} />
+      {/* Marquee rows */}
+<div className="relative overflow-hidden w-[95%] xs:w-[80%] mx-auto py-10 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+  <TopicRow items={row1} reverse={false} setHovered={setHoveredTopic} />
+  <TopicRow items={row2} reverse={true} setHovered={setHoveredTopic} />
+  <TopicRow items={row3} reverse={false} setHovered={setHoveredTopic} />
 
-        <HoveredVideoCard topic={hoveredTopic} />
+
+{/* Move video card outside this container */}
+<HoveredVideoCard topic={hoveredTopic} />
+
       </div>
     </div>
     </section>
---
+++ src/data/topics.js
@@ -0,0 +1,20 @@
+
+
+const topics = [
+    { name: "JavaScript", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", videoUrl: "https://www.youtube.com/watch?v=Hr5iLG7sUa0" },
+    { name: "Python", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg", videoUrl: "https://www.youtube.com/watch?v=Ca5DLSDfPec" },
+    { name: "Next.js", logo: "https://cdn.worldvectorlogo.com/logos/nextjs-2.svg", videoUrl: "https://www.youtube.com/watch?v=OgS1ZWZItno" },
+    { name: "Django", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg", videoUrl: "https://www.youtube.com/watch?v=j6szNSzw4BU" },
+    { name: "Docker", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg", videoUrl: "https://www.youtube.com/watch?v=rr9cI4u1_88" },
+    { name: "React.js", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", videoUrl: "https://www.youtube.com/watch?v=vz1RlUyrc3w" },
+    { name: "Kubernetes", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg", videoUrl: "https://www.youtube.com/watch?v=7XDeI5fyj3w" },
+    { name: "Node.js", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg", videoUrl: "https://www.youtube.com/watch?v=ooBxSg1Cl1w" },
+    { name: "HTML5", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", videoUrl: "https://www.youtube.com/watch?v=XmLOwJHFHf0" },
+    { name: "CSS3", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", videoUrl: "https://www.youtube.com/watch?v=aLzfFJb8rWo" },
+    { name: "Java", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg", videoUrl: "https://www.youtube.com/watch?v=Ev7-pchAFRM" },
+    { name: "C++", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg", videoUrl: "https://www.youtube.com/watch?v=gCkPJTSZ9mU" },
+    { name: "GoLang", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg", videoUrl: "https://www.youtube.com/watch?v=JoJ8Sw5Yb4c" },
+    { name: "Minecraft Server", logo: "https://cdn-icons-png.flaticon.com/512/5968/5968756.png", videoUrl: "https://www.youtube.com/watch?v=dZyQNy3-HjU" },
+  ];
+
+  export default topics
