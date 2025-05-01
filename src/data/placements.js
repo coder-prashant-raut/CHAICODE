df002fdcc6ea2f27f3bb895260cdc392cc6dd2bc
Prashant Raut <prashantraut1308@gmail.com>
2025-05-01 13:30:22 +0530

    added seprate file for const placements = [

---
+++ src/data/placements.js
@@ -0,0 +1,14 @@
+const placements = [
+    { student: "Aryan Mehta", role: "Cloud Engineer", company: "Google" },
+    { student: "Nisha Sharma", role: "Frontend Developer", company: "Facebook" },
+    { student: "Ravi Patel", role: "Software Engineer", company: "Microsoft" },
+    { student: "Sneha Jain", role: "SDE Intern", company: "Amazon" },
+    { student: "Kabir Singh", role: "ML Engineer", company: "Netflix" },
+    { student: "Divya Rathi", role: "DevRel Intern", company: "GitHub" },
+    { student: "Yash Gupta", role: "iOS Developer", company: "Apple" },
+    { student: "Aarushi Thakur", role: "Data Analyst", company: "Spotify" },
+    { student: "Anshul Verma", role: "Product Designer", company: "Notion" },
+    { student: "Megha Dube", role: "Backend Developer", company: "Slack" },
+  ];
+
+  export default placements;
